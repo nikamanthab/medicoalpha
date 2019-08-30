602b24c005031391a09a12124abf384e0888faae
ak5123 <kanduarul@gmail.com>
2019-08-30 15:08:30 +0530

    landing page and routing done

--- App.js
+++ App.js
@@ -26,97 +26,70 @@ import {
   DebugInstructions,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+import Pharm from "./container/pharm"
+import Diag from "./container/testcenter"
+import Home from "./container/home"
 
 // import BleManager from 'react-native-ble-manager';
+import {createSwitchNavigator, createAppContainer} from 'react-navigation'
+
+
+const AppSwitchNavigator = createSwitchNavigator({
+    Home: {screen: Home},
+    Pharm: {screen: Pharm },
+    Diag : {screen: Diag},
+});
+
+const AppContainer = createAppContainer(AppSwitchNavigator)
 
 class App extends Component{
 
-    startScan() {
-      BleManager.scan(["80-2B-F9-C6-A6-1F"], 10, true).then((results) => {
-        console.log('Scanning...');
-        console.log(results);
-      });
-    }
-
-  componentDidMount(){
- 
-    BleManager.start({showAlert: false}).then(() => {
-    // Success code
-    console.log('Module initialized');
-  });
-
-  console.log(Platform);
-
-  if (Platform.OS === 'android' && Platform.Version >= 23) {
-            PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION).then((result) => {
-                if (result) {
-                  console.log("Permission is OK");
-                } else {
-                  PermissionsAndroid.requestPermission(PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION).then((result) => {
-                    if (result) {
-                      console.log("User accept");
-                    } else {
-                      console.log("User refuse");
-                    }
-                  });
-                }
-          });
-        }
-
-  }
 
   render(){
     return(
-      <View style={{justifyContent:'center'}}>
-        <Text style={{textAlign: 'center'}}>
-           Hi there!!
-        </Text>
-        <TouchableHighlight style={{marginTop: 40,margin: 20, padding:20, backgroundColor:'#ccc'}} onPress={() => this.startScan()}>
-          <Text> Scan!!</Text>
-        </TouchableHighlight>
-      </View>
+      <AppContainer />
     )
   }
 }
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+// const styles = StyleSheet.create({
+//   scrollView: {
+//     backgroundColor: Colors.lighter,
+//   },
+//   engine: {
+//     position: 'absolute',
+//     right: 0,
+//   },
+//   body: {
+//     backgroundColor: Colors.white,
+//   },
+//   sectionContainer: {
+//     marginTop: 32,
+//     paddingHorizontal: 24,
+//   },
+//   sectionTitle: {
+//     fontSize: 24,
+//     fontWeight: '600',
+//     color: Colors.black,
+//   },
+//   sectionDescription: {
+//     marginTop: 8,
+//     fontSize: 18,
+//     fontWeight: '400',
+//     color: Colors.dark,
+//   },
+//   highlight: {
+//     fontWeight: '700',
+//   },
+//   footer: {
+//     color: Colors.dark,
+//     fontSize: 12,
+//     fontWeight: '600',
+//     padding: 4,
+//     paddingRight: 12,
+//     textAlign: 'right',
+//   },
+// });
 
 export default App;
 
